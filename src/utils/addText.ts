eaa9b0678823954dcea1c01c880c3863fd742cdd
JusticHentai <1360636180@qq.com>
2022-08-25 15:39:47 +0800

    feat: text

--- src/utils/addText.ts
+++ src/utils/addText.ts
@@ -2,9 +2,24 @@ import { Container, Text, TextStyle } from 'pixi.js'
 
 export interface Options {
   text: string
-  style?: {}
+  style?: {
+    fill: string // 字体颜色
+    fontSize?: number // 字体大小
+    breakWords?: boolean // 是否拆开单词换行
+    wordWrap?: boolean // 是否换行
+    wordWrapWidth?: number // 每行宽度
+    // debug 使用显示文案线条
+    textBaseline?:
+      | 'alphabetic'
+      | 'top'
+      | 'hanging'
+      | 'middle'
+      | 'ideographic'
+      | 'bottom'
+  }
   x?: number
   y?: number
+  debug?: boolean
 }
 
 /**
@@ -12,9 +27,12 @@ export interface Options {
  * @param options
  */
 export default function addText(options: Options): Container {
-  const { text, x = 0, y = 0, style } = options
+  const { text, x = 0, y = 0, style, debug = false } = options
 
-  const textStyle = new TextStyle(style)
+  const textStyle = new TextStyle({
+    textBaseline: debug ? 'bottom' : 'alphabetic',
+    ...style,
+  })
 
   const textContainer = new Text(text, textStyle)
 
